74ea188e800872014631e06849a892c9e864d08d
KimHH1 <150125279+KimHH1@users.noreply.github.com>
2023-12-07 21:12:38 +0900

    Add files via upload

---
+++ 20190814_1-master/MFCApplication 20190814/MFCApplication 20190814.h
@@ -0,0 +1,33 @@
+﻿
+// MFCApplication 20190814.h: MFCApplication 20190814 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CMFCApplication20190814App:
+// 이 클래스의 구현에 대해서는 MFCApplication 20190814.cpp을(를) 참조하세요.
+//
+
+class CMFCApplication20190814App : public CWinApp
+{
+public:
+	CMFCApplication20190814App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCApplication20190814App theApp;
---
+++ 20190814_1-master/MFCApplication 20190814/resource.h
@@ -0,0 +1,100 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MFCApplication20190814.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_MFCApplication20190814TYPE  130
+#define ID_WINDOW_MANAGER               131
+#define IDD_DIALOG_ANGLEINPUT           310
+#define IDC_EDIT1                       1000
+#define IDC_EDIT1_ANGLE                 1000
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_32773                        32773
+#define ID_32774                        32774
+#define ID_32771_ADD                    32775
+#define ID_32772_M                      32776
+#define ID_32773_MUL                    32777
+#define OnPixelAdd                      32778
+#define PixelAdd                        32779
+#define PixelMUL                        32780
+#define Pi                              32781
+#define PixelMNS                        32782
+#define PixelDIV                        32783
+#define PixelSUB                        32784
+#define ID_PIXEL_HISTO_EQ               32785
+#define ID_32786                        32786
+#define ID_PIXEL_CONTRAST_STRETCHING    32787
+#define ID_PIXEL_BINARIZATION           32788
+#define ID_PIXEL_TWO_IMAGE_ADD          32789
+#define ID_PIXEL_TWO_IMAGE_SUB          32790
+#define ID_32791                        32791
+#define ID_32792                        32792
+#define ID_32793                        32793
+#define ID_REGION_SMOOTING              32794
+#define ID_REGION_SMOOTHING             32795
+#define ID_REGION_SHARPENING            32796
+#define ID_REGION_EMBOSING              32797
+#define ID_REGION_EMBOSSING             32798
+#define ID_32799                        32799
+#define ID_32800                        32800
+#define ID_32801                        32801
+#define ID_REGION_PREWITT               32802
+#define ID_REGION_ROBERTS               32803
+#define ID_REGION_SOBEL                 32804
+#define ID_32805                        32805
+#define ID_REGION_AVERAGE_FILTERING     32806
+#define ID_32807                        32807
+#define ID_REGION_MEDIAN_FILTERING      32808
+#define ID_32809                        32809
+#define ID_MOPOLOGY_COLOR_GRAY          32810
+#define ID_MOPOLOGY_BINARIZATION        32811
+#define ID_32812                        32812
+#define ID_                             32813
+#define ID_32814                        32814
+#define ID_MOPOLOGY_EROSION             32815
+#define ID_MOPOLOGY_DILTION             32816
+#define ID_MOPOLOGY_DILATION            32817
+#define ID_Menu                         32818
+#define ID_MOPOLOGY_OPENING             32819
+#define ID_MOPOLOGY_CLOSING             32820
+#define ID_32821                        32821
+#define ID_GEOMETRY_ZOOMIN_PIXEL_COPY   32822
+#define ID_32823                        32823
+#define ID_GEOMETRY_ZOOMIN_BILINAR_INTERPO 32824
+#define ID_GEOMETRY_ZOOMIN_BILINAR_INTERPOLATION 32825
+#define ID_GEOMETRY_ZOOMIN_BILINEAR_INTERPOLATION 32826
+#define ID_GEOMETRY_ZOOMOUT_SUBSAMPLING 32827
+#define ID_32828                        32828
+#define ID_GEOMETRY_ZOOMOUT_MEAN_SUB    32829
+#define ID_32830                        32830
+#define ID_GEOMETRY_AVG_FILTERING       32831
+#define ID_GEOMETRY_SUBSAMPLING         32832
+#define ID_32833                        32833
+#define ID_GEOMETRY_ROTATION            32834
+#define ID_32835                        32835
+#define ID_GEOMETRY_HOLIZANTIAL_FUP     32836
+#define ID_32837                        32837
+#define ID_GEOMETRY_VERTICAL_FUP        32838
+#define ID_32839                        32839
+#define ID_GEOBETRY_WARPING             32840
+#define ID_32841                        32841
+#define ID_32842                        32842
+#define ID_GEOMETRY_MORPING             32843
+#define ID_GEOMETRY_MORPHING            32844
+#define ID_32845                        32845
+#define ID_AVI32846                     32846
+#define ID_AVI_VIEW                     32847
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32848
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
